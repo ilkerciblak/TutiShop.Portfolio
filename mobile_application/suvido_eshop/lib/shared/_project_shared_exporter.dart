75ba6b32868bdff230e5c838a665719fae1332c3
ilkerciblak <ilkerciblak@gmail.com>
2024-11-01 12:49:45 +0300

    refactor(shared): refactor shared exporter

--- mobile_application/suvido_eshop/lib/shared/_project_shared_exporter.dart
+++ mobile_application/suvido_eshop/lib/shared/_project_shared_exporter.dart
@@ -1,7 +1,9 @@
-export 'enums/_shared_enum_exporter.dart';
-export 'utils/_shared_utils_exporter.dart';
 export 'config/_shared_config_exporter.dart';
+export 'enums/_shared_enum_exporter.dart';
 export 'exceptions/_shared_exceptions_exporter.dart';
+export 'extensions/_shared_extensions_exporter.dart';
+export 'networking/network_manager.dart';
+export 'utils/_shared_utils_exporter.dart';
 export 'types/_shared_typedefs_exporter.dart';
 export 'theme/_shared_app_theme_exporter.dart';
 export 'widget/_shared_widget_exporter.dart';
